da666bdf43591d33df2a2019133a8b1d324d3074
adria <adria@REX54X>
2026-06-08 20:21:21 -0600

    Agregar archivos de proyecto.

---
+++ Mis finansas Consola/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mis_finansas_Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
